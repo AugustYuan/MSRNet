b8399997867955a6c3d4260322b328eaab316640
yuankun <yuankun@bytedance.com>
2019-10-30 11:17:09 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,12 @@ from .encoder import Encoder
 from .repair_net import RepairNets
 from .ssdecoder import SSDecoder
 from .msrmodel import MsrNet
+from .temmodel import temModel
 
 __all__ = [
     'Encoder',
     'RepairNets',
     'SSDecoder',
-    'MsrNet'
+    'MsrNet',
+    'temModel'
 ]
